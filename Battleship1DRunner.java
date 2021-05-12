6984e3f7969696b7654b04df2f9d17d690efdf43
GregoryMcCoy <74563594+charlesisafish@users.noreply.github.com>
2021-05-12 14:16:02 -0700

    Add files via upload

---
+++ Battleship1DRunner.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Battleship1DRunner here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Battleship1DRunner
+{
+    public static void main (String[] args){
+        Battleship1D game = new Battleship1D();
+        game.play();
+    }
+}
